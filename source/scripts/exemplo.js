04915c3a279bdfd74633829030df6f1b605ad38a
rafaelre1s <rafael.arnaldo@outlook.com>
2024-10-30 06:55:34 +0100

    first commit

---
+++ source/scripts/exemplo.js
@@ -0,0 +1,4 @@
+// para poder importar um função exportada de outro arquivo JS
+const somar = require('./sum');
+
+console.log('somar: ', somar(10, 20));
---
+++ source/scripts/main.js
@@ -0,0 +1,6 @@
+function somar(a, b) {
+    return a + b;
+}
+
+// para poder exportar uma função para outro arquivo JS
+module.exports = somar;
